4473e235e3c8d0f02a1028b73dd43f0deb0fa073
Alex Wilkinson <alexander.wilkinson.20@ucl.ac.uk>
2022-01-24 16:30:15 +0000

    making plot pdfs for presentation

--- plot_ndfd_pairs.py
+++ plot_ndfd_pairs.py
@@ -3,10 +3,11 @@ import os, argparse, sys
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib.lines import Line2D
+from matplotlib.backends.backend_pdf import PdfPages
 
 plt.rc('font', family='serif')
 
-def main(INPUT_DIR, N):
+def main(INPUT_DIR, N, NICE_PLOT):
   diffs = []
   for i, entry in enumerate(os.scandir(INPUT_DIR)):
     if not entry.name.endswith(".npy"):
@@ -22,6 +23,79 @@ def main(INPUT_DIR, N):
 
     arr_nd = arr_nd[16:-16, 58:-58]
     arr_fd = arr_fd[16:-16, 58:-58]
+
+    if NICE_PLOT: # Plot for presentations
+      if entry.name not in ['1ndfd.npy', '14ndfd.npy']:
+        continue
+
+      if entry.name == '1ndfd.npy':
+        pdf = PdfPages('1ndfd_evd.pdf')
+
+        arr_nd = np.roll(arr_nd, 52, 1)
+        arr_nd = arr_nd + np.roll(arr_nd, -1 ,1)
+
+        fig, ax = plt.subplots(1, 2, figsize=(16, 6), tight_layout=True)
+
+        ax[0].imshow(np.ma.masked_where(arr_nd == 0, arr_nd).T, cmap='viridis', aspect='auto', interpolation='none', origin='lower', vmin=0)
+        ax[0].set_title("ND", fontsize=16)
+        ax[0].set_xlabel("FD Wire", fontsize=14)
+        ax[0].set_ylabel("Tick [0.5us]", fontsize=14)
+        ax[0].set_xlim(60, 240)
+        ax[0].set_ylim(620, 2000)
+        ax[0].set_yticklabels([])
+        ax[0].set_xticklabels([])
+        ax[0].set_xticks([])
+        ax[0].set_yticks([]) 
+
+        ax[1].imshow(arr_fd.T, cmap='viridis', aspect='auto', interpolation='none', origin='lower')
+        ax[1].set_title("FD", fontsize=14)
+        ax[1].set_xlabel("FD Wire", fontsize=14)
+        ax[1].set_xlim(60, 240)
+        ax[1].set_ylim(620, 2000)
+        ax[1].set_yticklabels([])
+        ax[1].set_xticklabels([])
+        ax[1].set_xticks([])
+        ax[1].set_yticks([]) 
+
+        pdf.savefig(bbox_inches='tight')
+        pdf.close()
+        plt.close()
+
+        continue
+
+      if entry.name == '14ndfd.npy':
+        pdf = PdfPages('14ndfd_trace.pdf')
+
+        arr_nd = np.roll(arr_nd, 52, 1)
+
+        ch = (0, 0)
+        for i, col in enumerate(arr_nd):
+          if np.abs(col).sum() > ch[1]:
+            ch = (i, np.abs(col).sum())
+        ch = ch[0]
+
+        ticks_nd = arr_nd[ch, :]
+        ticks_fd = arr_fd[ch, :]
+        ticks = np.arange(1, arr_nd.shape[1] + 1)
+
+        fig, ax = plt.subplots(figsize=(16, 4),tight_layout=True)
+
+        ax.hist(ticks, bins=len(ticks), weights=ticks_nd, histtype='step', linewidth=0.7, color='b', label='ND')
+        ax.hist(ticks, bins=len(ticks), weights=ticks_fd, histtype='step', linewidth=0.7, color='r', label='FD')
+        ax.set_xlim(600, 900)
+        ax.set_ylabel("ADC", fontsize=14)
+        ax.set_xlabel("tick", fontsize=14)
+
+        handles, labels = ax.get_legend_handles_labels()
+        new_handles = [Line2D([], [], c=h.get_edgecolor()) for h in handles]
+        plt.legend(handles=new_handles, labels=labels, prop={'size': 12})
+
+        pdf.savefig(bbox_inches='tight')
+        pdf.close()
+        plt.close()
+
+        continue
+
     
     # Looking at tick shift (coming from somewhere inside WireCell that can de accessed in LArSoft so stuck with for now)
     # for i, (row_nd, row_fd) in enumerate(zip(arr_nd, arr_fd)):
@@ -39,7 +113,7 @@ def main(INPUT_DIR, N):
 
     # Manipulate array
     # arr_fd = np.flip(arr_fd, 1)
-    # # arr_fd = np.roll(arr_fd, 103, 1)
+    # # arr_nd = np.roll(arr_nd, 52, 1)
     # arr_nd_newres = np.zeros((480, 8984))
     # for i_ch, col in enumerate(arr_nd):
     #   for i_tick, adc in enumerate(col):
@@ -99,6 +173,7 @@ def parse_arguments():
     parser.add_argument("input_dir")
 
     parser.add_argument("-n", type=int, default=0, dest='n')
+    parser.add_argument("--nice_plot", action='store_true')
 
     # group1 = parser.add_mutually_exclusive_group()
     # group1.add_argument("--induction", action='store_true')
@@ -106,7 +181,7 @@ def parse_arguments():
 
     args = parser.parse_args()
 
-    return (args.input_dir, args.n)
+    return (args.input_dir, args.n, args.nice_plot)
 
 if __name__ == "__main__":
     arguments = parse_arguments()
