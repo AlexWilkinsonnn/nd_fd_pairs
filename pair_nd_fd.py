825857b445e97f4bdd270a58ed7fd950c6d96fd4
Alex Wilkinson <alexander.wilkinson.20@ucl.ac.uk>
2022-03-01 20:18:24 +0000

    option for non identical nd and fd detsim file ids

--- pair_nd_fd.py
+++ pair_nd_fd.py
@@ -2,10 +2,10 @@ import os, argparse, csv, re, sys
 
 import numpy as np
 
-def main(FD_DIR, ND_DIR, OUT_DIR, N): # Verify all is looking good then do matching
+def main(FD_DIR, ND_DIR, OUT_DIR, N, NOT_IDENTICAL): # Verify all is looking good then do matching
   N_fd = len(os.listdir(FD_DIR))
   N_nd = len(os.listdir(ND_DIR))
-  if N_fd != N_nd:
+  if not NOT_IDENTICAL and N_fd != N_nd:
     print("WARNING: fd_dir and nd_dir have a different number of files.")
     raise Exception
 
@@ -13,7 +13,22 @@ def main(FD_DIR, ND_DIR, OUT_DIR, N): # Verify all is looking good then do match
     os.makedirs(OUT_DIR)
 
   sorted_scandir = lambda dir : sorted(os.scandir(dir), key=lambda entry: entry.name)
-  for i, (entry_fd, entry_nd) in enumerate(zip(sorted_scandir(FD_DIR), sorted_scandir(ND_DIR))):
+  sorted_nd_dir = list(sorted_scandir(ND_DIR))
+  sorted_fd_dir = list(sorted_scandir(FD_DIR))
+
+  # Remove nd/fd entries without an fd/nd pair
+  if NOT_IDENTICAL:
+    fd_indices = { int(entry.name.split('_')[-1].split('.')[0]) for entry in sorted_fd_dir }
+    nd_indices = { int(entry.name.split('_')[-1].split('.')[0]) for entry in sorted_nd_dir }
+    fd_nd_indices = nd_indices.intersection(fd_indices)
+
+    sorted_nd_dir = [ entry for entry in sorted_nd_dir if int(entry.name.split('_')[-1].split('.')[0]) in fd_nd_indices ]
+    sorted_fd_dir = [ entry for entry in sorted_fd_dir if int(entry.name.split('_')[-1].split('.')[0]) in fd_nd_indices ]
+
+    N_nd = len(sorted_nd_dir)
+    N_fd = len(sorted_fd_dir) 
+
+  for i, (entry_fd, entry_nd) in enumerate(zip(sorted_fd_dir, sorted_nd_dir)):
     if N and i >= N:
       print("[{}/{}]".format(i, N_fd))
       break
@@ -59,13 +74,14 @@ def parse_arguments():
     parser.add_argument("out_dir")
 
     parser.add_argument("-n", type=int, default=0)
+    parser.add_argument("--not_identical", action='store_true')
     # group1 = parser.add_mutually_exclusive_group()
     # group1.add_argument("--induction", action='store_true')
     # group1.add_argument("--collection", action='store_true')
 
     args = parser.parse_args()
 
-    return (args.fd_dir, args.nd_dir, args.out_dir, args.n)
+    return (args.fd_dir, args.nd_dir, args.out_dir, args.n, args.not_identical)
 
 if __name__ == "__main__":
     arguments = parse_arguments()
